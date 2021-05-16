b8b959a1990e0a3be4ce449aee5a4ed4cf91c2b2
Subikesh <subikeshps@gmail.com>
2021-05-16 20:17:18 +0530

    Added documentation for ApiQuery interface

--- src/main/java/query/ApiQuery.java
+++ src/main/java/query/ApiQuery.java
@@ -3,6 +3,9 @@ package query;
 import java.util.*;
 import article.*;
 
+/**
+ * Interface contains the methods for creating, editing, and making API calls to newsAPI
+ */
 public interface ApiQuery {
     String domainUrl = "https://newsapi.org/v2/";
     String API_KEY = "27eee780f9e5438091906ffff0548b73";
@@ -24,7 +27,22 @@ public interface ApiQuery {
         put("lowerCase", "^[a-z]+$");
     }};
 
+    /**
+     * Validates the name and value and applies the filter to the API results
+     * @param name attributes for filter like language, sources, etc
+     * @param value value for the corresponding filter type - en, fr, it for languages
+     */
     void filterQuery(String name, String value);
+
+    /**
+     * Makes an API call for the filters made till then and stores the JSON in result
+     */
     void makeAPICall();
+
+    /**
+     * Returns result as objects
+     * @param <T> Object like News or Source
+     * @return a news or source object for the class in which it is implemented
+     */
     <T extends Article> List<T> getResult();
 }
