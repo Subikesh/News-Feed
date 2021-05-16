fadf571c3995df545044cad58e8df415654d7df9
Subikesh <subikeshps@gmail.com>
2021-05-16 20:02:53 +0530

    Validation for News' keys and values with regex

--- src/main/java/query/ApiQuery.java
+++ src/main/java/query/ApiQuery.java
@@ -1,11 +1,28 @@
 package query;
 
-import java.util.List;
+import java.util.*;
 import article.*;
 
 public interface ApiQuery {
     String domainUrl = "https://newsapi.org/v2/";
     String API_KEY = "27eee780f9e5438091906ffff0548b73";
+
+    // Contains all the key value pairs for regex validation
+    HashMap<String, String> REGEX_MAP = new HashMap<>()
+    {{
+        put("q", "^[a-z\\-\\.\\(\\)\\+\\\"]+$");
+        put("qlnTitle", "^[a-z\\-\\.\\(\\)\\+\\\"]+$");
+        put("sources", "^[a-z\\-]+$");
+        put("domains", "^[a-z\\.]+$");
+        put("excludeDomains", "^[a-z\\.]+$");
+        put("from", "^\\d{4}\\-\\d{2}\\-\\d{2}(T\\d{2}(:\\d{2}){2})?$");
+        put("to", "^\\d{4}\\-\\d{2}\\-\\d{2}(T\\d{2}(:\\d{2}){2})?$");
+        put("language", "[a-z]{2}");
+        put("sortBy", "^[a-z]+$");
+        put("pageSize", "\\d+");
+        put("page", "\\d+");
+        put("lowerCase", "^[a-z]+$");
+    }};
 
     void filterQuery(String name, String value);
     void makeAPICall();
--- src/main/java/query/NewsQuery.java
+++ src/main/java/query/NewsQuery.java
@@ -1,18 +1,39 @@
 package query;
 
 import article.Article;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+import java.util.*;
 
 public class NewsQuery implements ApiQuery {
-    String query;
-    Map<String, String> filterMap;
+    private String query;
+    public Map<String, String> filterMap;
+
+    // Contains all the possible filter values for the API
+    public final static Set<String> FILTER_KEYS = new HashSet<>(
+            Arrays.asList("q", "qlnTitle", "sources", "domains", "excludeDomains", "from", "to", "language",
+                    "sortBy", "pageSize", "page")
+    );
+
+    public NewsQuery() {
+        filterMap = new HashMap<>();
+        query = domainUrl + "everything?";
+
+        // Default filter is language: english
+        filterQuery("language", "en");
+    }
 
     @Override
-    public void filterQuery(String name, String value) {
-
+    public void filterQuery(@NotNull String name, @NotNull String value) throws RuntimeException{
+        // Validating name and value
+        // check if string is lowercase letters
+        if(!FILTER_KEYS.contains(name)) {
+            throw new RuntimeException("Invalid query parameter!");
+        }
+        // validates the given value for the corresponding name
+        if (!value.matches(REGEX_MAP.get(name))) {
+            throw new RuntimeException("Invalid query value!");
+        }
+        filterMap.put(name, value);
     }
 
     @Override
